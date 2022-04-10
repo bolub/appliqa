3cd6e4663f6925810f5e81e94ee902265b1bffae
bolub <abiol5202@gmail.com>
2022-04-10 20:46:09 +0100

    feat: :sparkles: Implement v1 for board drag n drop

--- utils/data.ts
+++ utils/data.ts
@@ -3,3 +3,24 @@ import { Options } from './GeneralProps';
 export const JOB_TYPES: Options[] = [
   { label: 'Remote Fulltime', value: 'remote_fulltime' },
 ];
+
+// interface taskProps{
+//   task : {
+//     id: string;
+//     content: string;
+//   }
+// }
+
+// interface columnProps {
+//   columns: {
+//     id: string;
+//     title: string;
+//     taskIds: string[]
+//   };
+// }
+
+// interface dataProps{
+//   tasks: taskProps[];
+//   columns: columnProps[];
+//   columnOrder: string[]
+// }
