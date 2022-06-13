caf98a23805ecab82f5d8c143f9f352c5890fba4
bolub <abiol5202@gmail.com>
2022-06-13 22:26:24 +0100

    Add seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,12 @@ import Footer from '../components/landing/Footer';
 import LandingHeader from '../components/landing/LandingHeader';
 import LandingNavbar from '../components/landing/LandingNavbar';
 import ManageProcess from '../components/landing/ManageProcess';
+import CustomSeo from '../components/UI/CustomSeo';
 
 const Home: NextPage = () => {
   return (
     <>
+      <CustomSeo />
       <LandingNavbar />
       <LandingHeader />
       <main>
