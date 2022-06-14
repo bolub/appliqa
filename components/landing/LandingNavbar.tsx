732003931ef32f0a33e8b88022dd689d1c6b21be
bolub <abiol5202@gmail.com>
2022-06-14 12:45:16 +0100

    feat: :sparkles: Display different button if logged in on landing page

--- components/landing/LandingNavbar.tsx
+++ components/landing/LandingNavbar.tsx
@@ -1,9 +1,21 @@
-import { Container, chakra, Button, HStack, Link } from '@chakra-ui/react';
+import {
+  Container,
+  chakra,
+  Button,
+  HStack,
+  Link,
+  Avatar,
+} from '@chakra-ui/react';
+import { getCookie } from 'cookies-next';
 import React from 'react';
-import { AUTH_ROUTES } from '../../utils/routes';
+import { useRecoilValue } from 'recoil';
+import { profileState } from '../../recoil/profile';
+import { AUTH_ROUTES, DASHBOARD_ROUTES } from '../../utils/routes';
 import Logo from '../UI/Logo';
 
 const LandingNavbar = () => {
+  const username = useRecoilValue(profileState)?.username;
+
   return (
     <chakra.nav
       bg='white'
@@ -17,13 +29,33 @@ const LandingNavbar = () => {
         <Logo containerProps={{ my: 'auto', mr: 20 }} />
 
         <HStack ml='auto' d={{ base: 'none', md: 'flex' }}>
-          <Link href={AUTH_ROUTES.LOGIN} isExternal>
-            <Button variant={'ghost'}>Login</Button>
-          </Link>
+          {getCookie('USER_AUTHENTICATED') ? (
+            <Link href={DASHBOARD_ROUTES.ANALYTICS} isExternal>
+              <Button variant={'ghost'}>
+                <Avatar
+                  w='36px'
+                  h='36px'
+                  p='1'
+                  bg='white'
+                  src={`https://avatars.dicebear.com/api/bottts/${
+                    username || getCookie('USER_NAME')
+                  }.svg`}
+                  mr={2}
+                />
+                My account
+              </Button>
+            </Link>
+          ) : (
+            <>
+              <Link href={AUTH_ROUTES.LOGIN} isExternal>
+                <Button variant={'ghost'}>Login</Button>
+              </Link>
 
-          <Link href={AUTH_ROUTES.SIGNUP} isExternal>
-            <Button colorScheme={'green'}>Get Started 😇</Button>
-          </Link>
+              <Link href={AUTH_ROUTES.SIGNUP} isExternal>
+                <Button colorScheme={'green'}>Get Started 😇</Button>
+              </Link>
+            </>
+          )}
         </HStack>
       </Container>
     </chakra.nav>
