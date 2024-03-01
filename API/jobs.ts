afe855f2ff0f56d527f185be164fdc474a6eef2f
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-03-01 13:31:32 +0200

    Remove unused

--- API/jobs.ts
+++ API/jobs.ts
@@ -18,39 +18,3 @@ export const fetchJobTips = async () => {
   const response = await API.get(`/job-tips`);
   return response.data;
 };
-
-export type hostintType = {
-  id: number;
-  name: string;
-  slug: string;
-  uid: string;
-  whmcs_pid: number;
-  product_url: string;
-  details: string[];
-} & Something;
-
-type Something =
-  | {
-      price?: any;
-    }
-  | {
-      product_pricing?: any;
-    };
-
-let test: hostintType = {
-  id: 1,
-  name: "hel",
-  slug: "hel",
-  uid: "hel",
-  whmcs_pid: 5,
-  product_url: "hel",
-  details: ["w", "wef"],
-};
-
-if ("price" in test) {
-  console.log(test.price);
-}
-
-if ("product_pricing" in test) {
-  console.log(test.product_pricing);
-}
